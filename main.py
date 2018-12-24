6288e9829c2041455230b1ee7e04ae6f93a13a3f
Aviad Hadad <aviadh@wix.com>
2018-12-24 12:48:42 +0200

    minor

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from graph_utils import *;
+from graph_utils import *
 from vsa import vsa
+import time
+
+
+def simple_becnh(fn):
+    start = time.time()
+    fn()
+    end = time.time()
+    print('total time:', end - start)
 
 
 def main():
-    graph = random_graph(20, 0.1)
+    graph = random_graph(3000, 0.1)
     np_graph = graph_to_numpy(graph)
     vertices = vsa(np_graph)
-    write_to_file('out.svg', graph, vertices)
+    # write_to_file('out.svg', graph, vertices)
     print(vertices)
+    print('length:', len(vertices))
 
 
 if __name__ == '__main__':
-    main()
+    simple_becnh(main)
